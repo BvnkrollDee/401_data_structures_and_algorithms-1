e8cc1fbc21dfe41abb6326ee71e4e5f52a630205
Daron Faulkner Jr <dj200099@gmail.com>
2023-07-13 11:42:14 -0500

    code challenge 1 complete

--- c-sharp/CodeChallenge2/ArrayInsertShift.cs
+++ c-sharp/CodeChallenge2/ArrayInsertShift.cs
@@ -4,8 +4,22 @@ namespace CodeChallenges
     {
         public static int[] InsertShiftArray(int[] array, int value)
         {
-            //insert the value into the middle of the array
-            return array;
+      //insert the value into the middle of the array
+
+      int[] newArray = new int[array.Length + 1];
+      int midIndex = array.Length / 2;
+      // loop
+      for (int i = 0; i < midIndex; i++)
+      {
+        newArray[i] = array[i];
+      }
+        newArray[midIndex] = value;
+      for (int i = midIndex + 1; i < newArray.Length; i++)
+      {
+        newArray[i] = array[i - 1];
+      }
+
+            return newArray;
         }
     }
 }
